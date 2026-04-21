42d72d80b7e2a4e7960c4cb36ee1bc65cfb18118
Danilo Silva <danilocgsilva@gmail.com>
2026-04-21 18:35:39 -0300

    Added basic html elements

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Index />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import Index from './components/Index.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    Index,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col gap-4 p-12 max-w-5xl mx-auto w-full">
+    <textarea
+      v-model="inputText"
+      placeholder="Type here..."
+      class="w-full h-40 p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 resize-none"
+    ></textarea>
+    <button class="w-full py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors">Ask</button>
+    <textarea
+      :value="outputText"
+      readonly
+      placeholder="Output will appear here..."
+      class="w-full h-96 p-3 rounded-lg border border-gray-200 bg-gray-50 text-gray-500 resize-none cursor-default"
+    ></textarea>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class Index extends Vue {
+  inputText = '';
+  outputText = '';
+}
+</script>
