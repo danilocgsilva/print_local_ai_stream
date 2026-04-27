da603fe23ed98bdf7978603de8a12d03aef15627
Danilo Silva <contact@danilocgsilva.me>
2026-04-26 22:22:16 -0300

    Changed the default port to 8099

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,14 +1,17 @@
 const { defineConfig } = require('@vue/cli-service');
 
+const port = parseInt(process.env.PORT) || 8099;
+
 module.exports = defineConfig({
   devServer: {
+    port,
     allowedHosts: 'all',
     webSocketServer: 'ws',
     client: {
       webSocketURL: {
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         pathname: '/ws',
-        port: parseInt(process.env.PORT) || 8080,
+        port,
         protocol: 'ws',
       },
     },
